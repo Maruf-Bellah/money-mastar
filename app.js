e9482b524c92b76f3ffaedebacd0a5bf3ad9e390
marufbellah <mdmaruf5511507@emai.com>
2022-03-01 21:37:00 +0600

    last-commit

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const toggleSpinner = displaySpinner =>{
      document.getElementById('spinner').style.display = displaySpinner;
 }
 
+const toggleInfo = displayInfo => {
+     document.getElementById('information').style.display = displayInfo;
+}
+
+const toggleError = displayError =>{
+     document.getElementById('error').style.display = displayError;
+}
+
 const searchButton = () =>{
      const inputText = document.getElementById('searchInput').value;
      document.getElementById('searchInput').value ='';
 
-     if(!inputText){
-          error.innerHTML=`<h5>Please Try Again</h5>`
-     } 
-     
-     const url = `https://openapi.programming-hero.com/api/phones?search=${inputText.slice(1, 20)}`
+     search.style.display="none"
+
+          const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`
           fetch(url)
           .then(res => res.json())
           .then(data => displayShow(data.data)) 
-          toggleSpinner('block')        
-
+          // display workd
+          toggleSpinner('block')    
+          toggleInfo('none')  
 
      
 }
@@ -23,35 +30,35 @@ const displayShow = phones => {
      const container = document.getElementById('phone-card');
      container.textContent = '';
           console.log(phones);
-          if(!phones){
+          if(!phones || phones == 0){
           error.innerHTML=`<h5>Please Try Again</h5>`
 
-          }                
-          for(const phone of phones.slice(0, 20)){
-               const div = document.createElement('div');
-               div.innerHTML=`
-               <div id="rounded"  class="card h-100 ">
-                     <img id="image" src="${phone.image}" class="card-img-top" alt="...">
-               <div class="card-body">
-                    <h5 class="card-title">${phone.phone_name}</h5>
-                    <h5 class="card-title">${phone.brand}</h5>
-                     <button onclick="displayDetails('${phone.slug}')" type="button" class="btn px-4 btn-primary rounded-pill">Detais</button>
-               </div>
-             </div>
-               `
-               container.appendChild(div);
           }
-                
+          else{
+               for(const phone of phones.slice(0, 20)){
+                    const div = document.createElement('div');
+                    div.innerHTML=`
+                    <div id="rounded"  class="card h-100 ">
+                          <img id="image" src="${phone.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                         <h5 class="card-title">${phone.phone_name}</h5>
+                         <h5 class="card-title">${phone.brand}</h5>
+                          <button onclick="displayDetails('${phone.slug}')" type="button" class="btn px-4 btn-primary rounded-pill">Detais</button>
+                    </div>
+                  </div>
+                    `
+                    container.appendChild(div);
+                    toggleError('none')
+               }
+                     
+          }
 
-                   
-        
+   
      
-     
-         
+          // display workd
+
           toggleSpinner('none')
-     
-
-
+          toggleInfo('block')
      }
 
 
@@ -68,20 +75,25 @@ const displayDetails = details =>{
 
 const displayShowDetails = info =>{
      const information = document.getElementById('information');
+     information.innerHTML ='';
      if(!info){
           error.innerHTML=`<h5>Please Try Again</h5>`
 
      }
-     information.innerHTML ='';
+     
      const div = document.createElement('div');
      div.innerHTML=`
      <div id="info" class="card my-5" style="max-width: 1000px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="${info.image}" class="img-fluid rounded-start ms-3 mt-4" alt="...">
+      <img src="${info.image}" class="img-fluid rounded-start ms-3 mt-3" alt="...">
+      <h6 class="ms-4 mt-1">Name : ${info.name} </h6>
+      <h6 class="ms-4 ">Brand : ${info.brand} </h6>
+      
+      
     </div>
     <div class="col-md-8">
-      <div class="card-body">
+      <div id="font" class="card-body">
         <h6 class="card-title">Release : ${info.releaseDate ? info.releaseDate: 'No Relase Date found'}</h6>
         <h6 class="card-title">Chip Set : ${info.mainFeatures.chipSet} </h6>
         <h6 class="card-title">Display Size : ${info.mainFeatures.displaySize}</h6>
